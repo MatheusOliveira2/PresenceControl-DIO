591355b6f473988cc18a7b8a0ee530a70828632d
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-07-06 11:55:02 -0300

    swagger config working

--- src/main/java/com/dio/presenceControl/swagger/SwaggerConfig.java
+++ src/main/java/com/dio/presenceControl/swagger/SwaggerConfig.java
@@ -21,10 +21,10 @@ public class SwaggerConfig {
     public Docket apiAdmin() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.dio.live"))
+                .apis(RequestHandlerSelectors.basePackage("com.dio.presenceControl"))
                 .paths(PathSelectors.ant("/**"))
                 .build()
-                .apiInfo(apiInfo())
+                .apiInfo(this.apiInfo())
                 .globalOperationParameters(
                         Collections.singletonList(
                                 new ParameterBuilder()
